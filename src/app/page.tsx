719faaf69cc68a4137bfd3fea3ad7488751da0a6
Martin <mr.mmaurer@gmail.com>
2025-09-23 00:46:03 +0200

    Add popover menu via command and commandfor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
+import { Menu } from "lucide-react";
+
 import "./styles.css";
 
 export default function Home() {
   return (
     <main>
       <h2>Home</h2>
+
+      <div className="example">
+        <button
+          // @ts-expect-error too-new
+          commandfor="menu"
+          command="show-popover"
+          aria-label="Show menu"
+          aria-haspopup="menu"
+        >
+          <Menu />
+        </button>
+        <ul popover="auto" id="menu">
+          <li>Link 1</li>
+          <li>Link 2</li>
+          <li>Link 3</li>
+        </ul>
+      </div>
     </main>
   );
 }
