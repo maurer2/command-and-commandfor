9b6db67948978eb606a53be170956873f7911a34
Martin <mr.mmaurer@gmail.com>
2025-12-22 21:37:43 +0100

    Checked out form handling in dialog element

--- src/app/modal/page.tsx
+++ src/app/modal/page.tsx
@@ -1,9 +1,45 @@
 import "./styles.css";
 
+export async function savePostData(formData: FormData) {
+  "use server";
+
+  console.log("Received form data:", Object.fromEntries(formData.entries()));
+}
+
 export default function ModalPage() {
   return (
     <main className="modal-page">
       <h2>Modal</h2>
+
+      <div className="example">
+        <button
+          type="button"
+          aria-haspopup="menu"
+          // @ts-expect-error too-new
+          commandfor="modal"
+          command="show-modal"
+        >
+          Open form in modal
+        </button>
+
+        <dialog id="modal" closedby="any">
+          {/* action overrides method="dialog" attribute in nextjs */}
+          <form method="dialog" action={savePostData}>
+            <label htmlFor="name">Name</label>
+            <input defaultValue="Test" name="name" id="name" />
+            <button type="submit">Submit</button>
+
+            <button
+              type="button"
+              // @ts-expect-error too-new
+              commandfor="modal"
+              command="close"
+            >
+              Close
+            </button>
+          </form>
+        </dialog>
+      </div>
     </main>
   );
 }
