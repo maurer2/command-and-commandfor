8f0eb1115b9009b3e14a44bde8da47caf58f907c
Martin <mr.mmaurer@gmail.com>
2025-12-28 23:48:08 +0100

    Remove erroneous semicolon

--- src/components/ModalDialog/components/ModalDialogTrigger/index.tsx
+++ src/components/ModalDialog/components/ModalDialogTrigger/index.tsx
@@ -3,5 +3,5 @@ import type { PropsWithChildren } from "react";
 type ModalDialogTrigger = PropsWithChildren;
 
 export function ModalDialogTrigger({ children }: ModalDialogTrigger) {
-  return <>{children};</>;
+  return <>{children}</>;
 }
